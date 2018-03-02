af07369347f195475424daecfb7699dec86ee855
hefei00 <hefei00@outlook.com>
2018-03-02 22:49:03 +0800

    readme & comments
    Signed-off-by: hefei00 <hefei00@outlook.com>

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # react starter
+a easy react starter base on create-react-app
+## what's inside?
+* create-react-app, base starter
+* react-scripts-rewired, add custom config to CRA
+* prettier, format code automatically
 ## how to use
 ```bash
 git clone git@github.com:hefei00/react-starter.git my_obj
--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,21 @@
 const { injectBabelPlugin, getLoader } = require('react-app-rewired')
 
 module.exports = function override(config, env) {
+  //inject babel, antd 按需加载
   config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config)
 
+  //get css loader
   const cssLoader = getLoader(
     config.module.rules,
     rule => rule.test && String(rule.test) === String(/\.css$/)
   );
 
+  //sass loader
   const sassLoader = {
     test: /\.scss$/,
     use: [...(cssLoader.loader || cssLoader.use), 'sass-loader']
   };
-
+  //add sass loader to webpack.config
   const oneOf = config.module.rules.find(rule => rule.oneOf).oneOf;
   oneOf.unshift(sassLoader);
   return config
