0166449b71588f81d3c1602b2ceaaa39b01bdf10
hefei00 <hefei00@outlook.com>
2018-03-28 10:22:47 +0800

    add  comments

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,24 @@
 const { injectBabelPlugin, getLoader } = require('react-app-rewired')
 
+//针对create-react-app添加自定义配置，常见配置社区已经写好插件
+//https://github.com/timarney/react-app-rewired
 module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config)
+  //按需加载，以antd为例 需要npm install babel-plugin-import antd
+  // config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config)
 
-  const cssLoader = getLoader(
-    config.module.rules,
-    rule => rule.test && String(rule.test) === String(/\.css$/)
-  );
+  //添加自定义webpack配置，以sass为例，需要npm install node-sass sass-loader
+  // const cssLoader = getLoader(
+  //   config.module.rules,
+  //   rule => rule.test && String(rule.test) === String(/\.css$/)
+  // );
 
-  const sassLoader = {
-    test: /\.scss$/,
-    use: [...(cssLoader.loader || cssLoader.use), 'sass-loader']
-  };
+  // const sassLoader = {
+  //   test: /\.scss$/,
+  //   use: [...(cssLoader.loader || cssLoader.use), 'sass-loader']
+  // };
 
-  const oneOf = config.module.rules.find(rule => rule.oneOf).oneOf;
-  oneOf.unshift(sassLoader);
+  // const oneOf = config.module.rules.find(rule => rule.oneOf).oneOf;
+  // oneOf.unshift(sassLoader);
+
   return config
 }
