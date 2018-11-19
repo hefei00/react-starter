bf1cae4a9facfd402151256ba3527eb3685fbb55
hefei00 <hefei00@outlook.com>
2018-11-19 14:36:21 +0800

    feat: add a didCatch life-circle to app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Router, Link } from '@reach/router'
 import Home from './pages/home'
 import About from './pages/about'
 import Count from './pages/count'
 
-const App = () => (
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/count">Count</Link>
-        </li>
-      </ul>
-    </nav>
-    <hr />
-    <Router>
-      <Home path="/" />
-      <About path="/about" />
-      <Count path="/count" />
-    </Router>
-  </div>
-)
-
-export default App
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasError: false
+    }
+  }
+  static getDerivedStateFromError(error) {
+    return {
+      hasError: true
+    }
+  }
+  componentDidCatch(error, info) {
+    console.log(error, info)
+    // TODO: report the errors
+  }
+  render() {
+    return (
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/count">Count</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        <Router>
+          <Home path="/" />
+          <About path="/about" />
+          <Count path="/count" />
+        </Router>
+      </div>
+    )
+  }
+}
